b9b31911048d5ad1b4073a962d470aab715cb8e2
dorian m <pro.dorianm@gmail.com>
2023-07-11 18:26:11 +0200

    ajout de lenvoi email

--- src/mailer/mail.php
+++ src/mailer/mail.php
@@ -1,43 +1,50 @@
 <?php
 
+require 'src/mailer/phpMailer/src/PHPMailer.php';
+require 'src/mailer/phpMailer/src/SMTP.php';
+require 'src/mailer/phpMailer/src/Exception.php';
+
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'phpmailer/PHPMailer-master/src/Exception.php';
-require 'phpmailer/PHPMailer-master/src/PHPMailer.php';
-require 'phpmailer/PHPMailer-master/src/SMTP.php';
-
-if(isset($_POST['send'])){
-    $to = "pro.dorianm@gmail.com";
+function sendEmail($recipient, $subject, $content, $name) {
+    $mail = new PHPMailer(true);
+    $recipient = $_POST['email'];
     $subject = $_POST['subject'];
-    $name = $_POST['name'];
-    $from = $_POST['email'];
-    $msg = $_POST['message'];
-    $subMsg = 'address email de l\'expediteur '. $from;
+    $content  = $_POST['name']. '<br> <br>' .$_POST['message'];
+    try {
+        // SMTP settings
+        $mail->isSMTP();
+        $mail->Host = 'smtp-relay.brevo.com';  // SMTP server address
+        $mail->SMTPAuth = true;
+        $mail->Username = 'pro.dorianm@gmail.com';  // SMTP authentication email address
+        $mail->Password = 'JU0gm9OXPkyAMdv5';  // SMTP authentication password
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
 
-    if(isset($_POST['email']) && isset($_POST['message']) && isset($_POST['name'])) {
-        try {
-            $mail = new PHPMailer;
-            $mail->isSMTP();
-            $mail->Host ='smtp.gmail.com';
-            $mail->SMTPAuth = true;
-            $mail->Username = 'pro.dorianm@gmail.com';
-            $mail->Password = 'azkccyczbrqywtkl';
-            $mail->SMTPSecure = 'tls';
-            $mail->Port = 587;
-            $mail->setFrom($from);
-            $mail->addAddress($to);
-            $mail->isHTML(true);
-            $mail->Subject = $subject;
-            $mail->Body = $msg. $subMsg . $name;
-            $mail->send();
-            header('Location: https://dorianmarechal.com/?#contact');
-            exit();
-        } catch (Exception $e) {
-            $error_msg = $e->getMessage();
-            // TODO: REDIRIGER VERS UNE PAGE D ERREUR
-            exit();
-        }
+        // Recipient and email content
+        $mail->setFrom('pro.dorianm@gmail.com', 'Dorian Marechal');
+        $mail->addAddress($recipient);
+        $mail->Subject = $subject;
+        $mail->Body = $content;
 
+        // Send the email
+        $mail->send();
+        return true;
+    } catch (Exception $e) {
+        return false;
     }
+}
+
+// Example usage of the function
+$recipient = $_POST['email'];
+$subject = $_POST['subject'];
+$content  = $_POST['name']. '<br> <br>' .$_POST['message'];
+$name = $_POST['name'];
+
+if (sendEmail($recipient, $subject, $content, $name)) {
+    echo 'Email sent successfully!';
+} else {
+    echo 'Error sending email.';
 }
